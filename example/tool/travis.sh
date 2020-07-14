49dcaa994256153e984bcee7976cd3c2c91566ea
simphotonics <git@simphotonics.com>
2020-07-14 23:11:06 +0100

    Remove pub get and pub upgrade.

--- example/tool/travis.sh
+++ example/tool/travis.sh
@@ -20,16 +20,6 @@ echo
 echo -e "${CYAN}=== Preparing Example $PWD...${RESET}"
 echo
 
-# Resolving dependencies
-echo
-echo -e "${BLUE}=== Resolving Dependencies $PWD...${RESET}"
-echo
-
-# Make sure .dart_tool/package_config.json exists.
-pub get
-
-# Upgrade packages.
-pub upgrade
 
 echo
 echo -e "${PURPLE}=== Checking Source Code Formatting${RESET} $PWD..."
@@ -47,8 +37,7 @@ echo
 dartanalyzer \
     --fatal-warnings \
     --fatal-infos \
-    --packages="$PWD/.packages" \
-    $(find bin lib test -name \*.dart 2>/dev/null)
+     $(find bin lib test -name \*.dart 2>/dev/null)
 
 
 # Running benchmark
